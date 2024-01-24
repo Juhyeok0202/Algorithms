e85089177d1a8bf51f984e3c451159febb4b7f9a
Limwngur <ovg07047@naver.com>
2024-01-24 14:47:48 +0900

    Implementation: 백준2563(FIX)

--- src/Implementation/백준/P_2563RE.java
+++ src/Implementation/백준/P_2563RE.java
@@ -11,19 +11,22 @@ public class P_2563RE {
         int total = 0;
         int n = Integer.parseInt(br.readLine());
         boolean[][] arr = new boolean[101][101];
+
         for (int i = 0; i < n; ++i) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
+
             for (int j = x; j < x + 10; ++j) {
                 for (int k = y; k < y + 10; ++k) {
-                    if (!arr[i][k]) {
-                        arr[i][k] = true;
+                    if (!arr[j][k]) {
+                        arr[j][k] = true;
                         total++;
                     }
                 }
             }
         }
+
         System.out.println(total);
     }
 }
